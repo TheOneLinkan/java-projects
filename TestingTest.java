42fd66e47929e770f20b8d576416d19813565d68
Linus Sandelin <thelinussandelin@gmail.com>
2025-09-10 16:21:32 +0200

    Update TestingTest.java

--- TestingTest.java
+++ TestingTest.java
@@ -2,6 +2,7 @@ package se.jensen.linkan.testingtest;
 
 public class TestingTest {
     public static void main(String[] args) {
-        System.out.println("Tja Johanna!");
+        System.out.println("Tja!");
     }
 }
+
